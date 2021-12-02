23958c8614417b57fd3818120280ef928326c1ed
Pascal Seyler <pascal.seyler@rwth-aachen.de>
2021-12-02 14:26:22 +0100

    add Day1.java and Day1.txt and switch case in Main for start Programms

---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter day to solve:");
+        int day = sc.nextInt();
+        switch (day){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            default:
+        }
+    }
+}
